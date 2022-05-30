93413ad453d9573dd1cdb1060741009f49892b2e
atxoupi <andres.hermelo@gmail.com>
2022-05-30 19:35:59 +0000

    falta hacer que al encender una luz se apaguen el resto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+	function cambiaColor() {
+		color === "" ? setColor("ilumina") : setColor("");
+	}
+
+	const [color2, setColor2] = useState("");
+	function cambiaColor2() {
+		color2 === "" ? setColor2("ilumina") : setColor2("");
+	}
+
+	const [color3, setColor3] = useState("");
+	function cambiaColor3() {
+		color3 === "" ? setColor3("ilumina") : setColor3("");
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<h1 className="text-center mt-5">Semaforo!</h1>
+			</div>
+			<div id="semaforo" className="bg-dark d-flex-column m-auto">
+				<div
+					id="verde"
+					className={"rounded-circle m-auto " + color}
+					onClick={cambiaColor}></div>
+				<div
+					id="naranja"
+					className={"rounded-circle m-auto " + color2}
+					onClick={cambiaColor2}></div>
+				<div
+					id="rojo"
+					className={"rounded-circle m-auto " + color3}
+					onClick={cambiaColor3}></div>
+			</div>
+		</>
 	);
 };
 
