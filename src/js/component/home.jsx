33a8cf8f8da7da99ac99cb1b012bc202ad3d9dc4
atxoupi <andres.hermelo@gmail.com>
2022-06-08 10:30:09 +0000

    Funcionamiento independiente

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,16 +8,22 @@ const Home = () => {
 	const [color, setColor] = useState("");
 	function cambiaColor() {
 		color === "" ? setColor("ilumina") : setColor("");
+		setColor2("");
+		setColor3("");
 	}
 
 	const [color2, setColor2] = useState("");
 	function cambiaColor2() {
 		color2 === "" ? setColor2("ilumina") : setColor2("");
+		setColor("");
+		setColor3("");
 	}
 
 	const [color3, setColor3] = useState("");
 	function cambiaColor3() {
 		color3 === "" ? setColor3("ilumina") : setColor3("");
+		setColor("");
+		setColor2("");
 	}
 
 	return (
@@ -25,18 +31,20 @@ const Home = () => {
 			<div>
 				<h1 className="text-center mt-5">Semaforo!</h1>
 			</div>
-			<div id="semaforo" className="bg-dark d-flex-column m-auto">
+			<div
+				id="semaforo"
+				className="bg-dark d-flex-column m-auto p-2 rounded-3">
 				<div
 					id="verde"
-					className={"rounded-circle bg-success m-auto " + color}
+					className={"rounded-circle bg-success m-auto p-3 " + color}
 					onClick={cambiaColor}></div>
 				<div
 					id="naranja"
-					className={"rounded-circle bg-warning m-auto " + color2}
+					className={"rounded-circle bg-warning m-auto p-2" + color2}
 					onClick={cambiaColor2}></div>
 				<div
 					id="rojo"
-					className={"rounded-circle bg-danger m-auto " + color3}
+					className={"rounded-circle bg-danger m-auto p-2" + color3}
 					onClick={cambiaColor3}></div>
 			</div>
 		</>
